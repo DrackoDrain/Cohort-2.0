7992f4e13853f7046adf8df455282880e82a9b32
Ayush <ayush0999aryan@gmail.com>
2023-12-06 21:46:47 +0530

    6th dec23

---
+++ 0-1/Week 1 JS/Assignment-1/main1.js
@@ -0,0 +1,7 @@
+// Create a counter in Javascript (count down from 30 to 0)
+
+
+// for (let i =30 ; i>0; i--){
+//     console.log(i);
+// }
+
--- 0-1/Week 1 JS/Week1.2/test.js
+++ 0-1/Week 1 JS/Week1.2/test.js
@@ -4,7 +4,7 @@ let school = {
     established : '1971', 
     displayInfo : function(){ 
         console.log(`${school.name} was established  
-              in ${school.established} at ${school.location}`); 
+                     in ${school.established} at ${school.location}`); 
     } 
 } 
 school.displayInfo();    
