66dd139d24c88a59e4254ce74726770003ef046b
Ayush <ayush0999aryan@gmail.com>
2023-12-13 00:56:02 +0530

    math-js

---
+++ 0-1/Week 1 JS/Week1.3/level-2/math.js
@@ -0,0 +1,15 @@
+console.log(Math.random());
+console.log(Math.ceil(2.9)); // gives biggest integer
+console.log(Math.floor(3.8)); // gives the lowest integer
+console.log(Math.max(5,10,15));
+
+
+let minValue = Math.min(6,16,26);
+console.log(minValue);
+
+
+let powerOfTwo = Math.pow(10,2);
+console.log("after pow():", powerOfTwo);
+
+let squareRoot = Math.sqrt(100);
+console.log("Square root ():", squareRoot);
