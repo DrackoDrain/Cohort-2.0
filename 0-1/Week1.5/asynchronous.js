041a19fe7b1b8396ecb73df884e1dc48fbbbbe50
Ayush <ayush0999aryan@gmail.com>
2023-12-14 14:12:49 +0530

    asynchronous function

---
+++ 0-1/Week1.5/asynchronous.js
@@ -0,0 +1,21 @@
+// Asynchronous function -> Happens in parts, or deligate the works
+
+function findSumTill(n){
+    let a =0;
+    for (let i = 0; i < n ; i++){
+        a += i;
+    }
+    console.log(a);
+  }
+  // console.log(findSumTill(9));
+  
+  function findSum(){
+      return findSumTill(100);
+  }
+  
+  
+  setTimeout(findSum, 1000);
+
+  console.log("Hello world")
+
+
