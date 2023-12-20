509116ef5a0ac3cc1ecdf769a1318a7d4385d412
Ayush <ayush0999aryan@gmail.com>
2023-12-20 19:31:45 +0530

    Promise

---
+++ 0-1/Week1.5/test.js
@@ -0,0 +1,15 @@
+// Promise 
+
+
+let promise = new Promise(function (resolve, reject) {
+	reject('Promise Rejected')
+})
+
+promise
+	.then(function (successMessage) {
+		console.log(successMessage);
+	})
+	.catch(function (errorMessage) {
+		//error handler function is invoked 
+		console.log(errorMessage);
+	});
